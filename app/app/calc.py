9079905f6d82b911ed50d4025ea9c8b36428b6c5
Abhinav <35560638+AbhinavJD@users.noreply.github.com>
2023-10-05 16:40:37 +0100

    postgresql db configuration added to docker and initial test case added

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """
+        Add x and y and return the value
+    """
+    return x + y
+
+def substract(x, y):
+    """
+        Substract x and y and return the value
+    """
+    return x - y
